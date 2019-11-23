daefdbcbdf3f901591306d0fdad28831bf872f10
aparnada7 <aparna20dalal@gmail.com>
2019-11-23 13:13:22 -0800

    Preference based search-resolved string parsing in mongo query

--- routes/search.js
+++ routes/search.js
@@ -6,19 +6,25 @@ var Features = require("../models/features");
 
 router.post("/search", function(req, res) {
     console.log("Inside search info request:");
+    // console.log(req);
     console.log("City : ", req.body.city);
     console.log("Preference 1: ", req.body.preference1)
-    let city = JSON.stringify(req.body.city);
-    let preference1 = JSON.stringify(req.body.preference1);
+    // let request = JSON.parse(req);
+    // console.log("----> ", request);
+    // let city = JSON.stringify(req.body.city);
+    // let preference1 = JSON.stringify(req.body.preference1);
+    let city = req.body.city;
+    let preference1 = req.body.preference1;
     console.log("String City : ", city, preference1);
-
     // console.log("String Preference 1: ", req.body.preference1)
     // {$and: [{"stars":{$gte: 4.5} }, {"city":"Las Vegas"}, {"name": {$regex:"Fitness", $options:"$i"}   }]}
     //   Features.findOne({$and: [{"stars":{$gte: 4.5} }, {"city":city}, {} ]}, function(err, data){
-    Features.findOne({$and: [{"stars":{$gte: 4.5} }, {"city":city}, {"name": {$regex: preference1, $options:"$i"}   }]}, function(err, data){
+    //Sample Mongo query:
+    // db.features.find({$and: [{"stars":{$gte: 4.5} }, {"city":"Scottsdale"}, {"name": {$regex:"Shop", $options:"$i"}   }]});
+    Features.findOne({$and: [{"stars":{$gte: 4.5} }, {"city":city}, {"name": {$regex:preference1, $options:"$i"}   }]}, function(err, data){
         if(err){
         console.log(err);
-        throw err
+        res.send({})
         }
         else{
             console.log("Data fetched from features collection:", data)
